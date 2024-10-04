114e283e9289256a1257fa67c8afcf2f818a2d12
Travis <181103286+travsta@users.noreply.github.com>
2024-10-04 15:31:09 +0200

    Update dummy_model.py

--- src/model/dummy_model.py
+++ src/model/dummy_model.py
@@ -35,6 +35,7 @@ class DummyTopicClassifier:
         return self.topics
 
 if __name__ == "__main__":
+    # for local testing
     classifier: DummyTopicClassifier = DummyTopicClassifier()
     sample_text: str = "This is a sample Instagram post."
     result: Dict[str, float] = classifier.predict(sample_text)
